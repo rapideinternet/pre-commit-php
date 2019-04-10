2323cbcda8aee49d77c1ed7e8b486badff22f445
Peter Mein <petermein@gmail.com>
2019-04-10 12:24:12 +0200

    Update php-cs.sh

--- pre_commit_hooks/php-cs.sh
+++ pre_commit_hooks/php-cs.sh
@@ -1,46 +1,13 @@
 #!/usr/bin/env bash
+echo "php-cs-fixer pre commit hook start"
 
-EXECUTABLE_NAME=php-cs-fixer
-EXECUTABLE_COMMAND=fix
-CONFIG_FILE=.php_cs
-CONFIG_FILE_PARAMETER='--config-file'
-ROOT=`pwd`
+PHP_CS_FIXER="bin/php-cs-fixer"
+PHP_CS_CONFIG=".php_cs"
+CHANGED_FILES=$(git diff --cached --name-only --diff-filter=ACM -- '*.php')
 
-# possible locations
-locations=(
-  $ROOT/bin/$EXECUTABLE_NAME
-  $ROOT/vendor/bin/$EXECUTABLE_NAME
-  `which $EXECUTABLE_NAME`
-)
-
-for location in ${locations[*]}
-do
-  if [[ -x $location ]]; then
-    EXECUTABLE=$location
-    break
-  fi
-done
-
-if [[ ! -x $EXECUTABLE ]]; then
-  echo "executable $EXECUTABLE_NAME not found, exiting..."
-  echo "if you're sure this is incorrect, make sure they're executable (chmod +x)"
-  exit
+if [ -n "$CHANGED_FILES" ]; then
+    $PHP_CS_FIXER fix --config "$PHP_CS_CONFIG" $CHANGED_FILES;
+    git add $CHANGED_FILES;
 fi
 
-echo "using \"$EXECUTABLE_NAME\" located at $EXECUTABLE"
-$EXECUTABLE --version
-
-if [[ -f $ROOT/$CONFIG_FILE ]]; then
-  CONFIG=$ROOT/$CONFIG_FILE
-  echo "config file located at $CONFIG loaded"
-fi
-
-git status --porcelain | grep -e '^[AM]\(.*\).php$' | cut -c 3- | while read line; do
-  if [[ -f $CONFIG ]]; then
-    $EXECUTABLE $EXECUTABLE_COMMAND $CONFIG_FILE_PARAMETER=$CONFIG $line;
-  else
-    $EXECUTABLE $EXECUTABLE_COMMAND $line;
-  fi
-
-  git add $line;
-done
+echo "php-cs-fixer pre commit hook finish"
